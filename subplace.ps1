b177b42abc42b0d4fabd6c5cd564b45aa4bc52d1
kit/zyn ✯ <77354428+catb0x@users.noreply.github.com>
2024-11-15 15:11:07 +0100

    update 6!!!1 yay i thjink
    subplace.ps1 next update goals:
    - ~~reworked settings~~ DONE
    - ~~reworked script? not comepletely but made more organized and better ig~~ DONE
    - ~~ manual mode ~~ KINDA DONE?
    - ~~ FORGOT TO SAY: gsudo support ~~ DONE
    - ADDITION: ~~better script functionality, being able to leave when saying nvm? i think~~  DONE
    - ~~ ADDITION: settings saving & loading~~  DONE

--- subplace.ps1
+++ subplace.ps1
@@ -1,11 +1,35 @@
 <#
 subplace.ps1
-by kit
-version 5 !!! NOt release candidate oops this is final v5
-:3
+by kit (https://github.com/catb0x/subplace.ps1)
+version 6 ^_^
+subject to the terms of the MPL 2.0, you can get a copy at http://mozilla.org/MPL/2.0/
 #>
 
 [CmdletBinding()] param ()
+
+$jsonPath = "$env:LOCALAPPDATA\subplace\settings.json"
+
+if (Get-Command "gsudo" -errorAction SilentlyContinue) {
+    $gsudoExists = $true
+} else {
+	$gsudoExists = $false
+}
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
+
+if (!($isAdmin) -and !($gsudoExists)) {
+	Write-Warning "script is not running as administrator. relaunching with elevated permissions..."
+	Start-Process powershell -ArgumentList "-NoExit", "-noprofile", "-executionpolicy bypass", "-file `"$PSCommandPath`"" -Verb RunAs
+	exit
+} elseif (!($isAdmin) -and ($gsudoExists)) {
+	Write-Verbose "gsudo mode automatically enabled,,,"
+	$gsudoMode = 2
+} elseif (($isAdmin) -and ($gsudoExists)) {
+	Write-Verbose "gsudo mode can be enabled if macros break,,,"
+	$gsudoMode = 1
+} else {
+	Write-Warning "download gsudo if not running other macros as admin,,,"
+	$gsudoMode = 0
+}
 
 function Is-WindowInForeground {
     $windowName = $args[0]
@@ -13,12 +37,88 @@ function Is-WindowInForeground {
 	return $windowExists
 }
 
-$wshell = New-Object -ComObject wscript.shell
+function Ask {
+    $question = Read-Host $args[1]
+	if ($question -eq "?") {
+		Write-Host $args[2] -ForegroundColor Blue
+		Ask $args[0] $args[1] $args[2]
+	} elseif ($question -ne "") {
+		Set-Variable -Name $args[0] -Value $question -Scope Script
+	}
+}
 
-if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Output "Script is not running as Administrator. Relaunching with elevated permissions..."
-    Start-Process powershell -ArgumentList "-NoExit", "-noprofile", "-executionpolicy bypass", "-file `"$PSCommandPath`"" -Verb RunAs
-    exit
+function BooleanAsk {
+    $question = Read-Host $args[1]
+	$booleanAnswer = $null
+	if ($question -eq "?") {
+		Write-Host $args[2] -ForegroundColor Blue
+		BooleanAsk $args[0] $args[1] $args[2]
+	} elseif ([bool]::TryParse($question, [ref]$booleanAnswer)) {
+		Set-Variable -Name $args[0] -Value $booleanAnswer -Scope Script
+	}
+}
+
+function IntAsk {
+    $question = Read-Host $args[1]
+	if ($question -eq "?") {
+		Write-Host $args[2] -ForegroundColor Blue
+		BooleanAsk $args[0] $args[1] $args[2]
+	} elseif ($question -match "^\d+$") {
+		Set-Variable -Name $args[0] -Value ([int]$question) -Scope Script
+	}
+}
+
+function JobId {
+    $question = Read-Host "instance/job id? (leave blank if none)"
+	if ($question -eq "nvm") {
+		Write-Host "not continuing..." -ForegroundColor Green
+		$script:continue = $false
+	} elseif ($question -ne "") {
+		$other = "&gameInstanceId=${$question}"
+	}
+}
+
+function SaveSettings {
+    param (
+		[Parameter(Position=0)]
+		[string[]]$VariableNames
+	)
+    $varHash = @{}
+    foreach ($varName in $VariableNames) {
+        if (Get-Variable -Name $varName -ErrorAction SilentlyContinue) {
+            $varHash[$varName] = (Get-Variable -Name $varName).Value
+        }
+    }
+    New-Item -Path (Split-Path $jsonPath) -ItemType Directory -Force | Out-Null
+    $varHash | ConvertTo-Json | Set-Content $jsonPath
+}
+
+function LoadSettings {
+    if (Test-Path $jsonPath) {
+        $varHash = Get-Content $jsonPath | ConvertFrom-Json
+        foreach ($key in $varHash.PSObject.Properties.Name) {
+            Set-Variable -Name $key -Value $varHash.$key -Scope Script -Force
+        }
+        Write-Verbose "settings loaded..."
+    } else {
+        Write-Verbose "settings not found... making them rn"
+        $script:loop = 300
+        $script:sleep = 100
+        $script:manual = $false
+        $script:gsudoOptional = $false
+        SaveSettings "loop", "sleep", "manual", "gsudoOptional"
+    }
+}
+LoadSettings
+
+$defPath = (Get-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\roblox-player\shell\open\command").'(default)' -replace '^"(.+?)".+', '$1'
+if ($defPath -eq "$($env:LOCALAPPDATA)\Bloxstrap\Bloxstrap.exe") {
+	$bootstrapper = "Bloxstrap"
+	$roblox = "$($env:LOCALAPPDATA)\Bloxstrap\Roblox\Player\RobloxPlayerBeta.exe"
+} else {
+	$bootstrapper = "Roblox"
+	$roblox = $defPath
+	Write-Warning "this will probably not work w/o bloxstrap lol"
 }
 
 Write-Host "            ___.          .__                                         ____ 
@@ -28,85 +128,81 @@ Write-Host "            ___.          .__                                       
 /____  >____/|___  /   __/|____(____  /\___  >___  > /\ |   __/____  >|___|
      \/          \/|__|             \/     \/    \/  \/ |__|       \/      " -ForegroundColor Blue
 
-$robloxPath = (Get-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\roblox-player\shell\open\command").'(default)' -replace '^"(.+?)".+', '$1'
-if ($robloxPath -eq "$($env:LOCALAPPDATA)\Bloxstrap\Bloxstrap.exe") {
-	$bootstrapper = "Bloxstrap"
-	$roblox = "$($env:LOCALAPPDATA)\Bloxstrap\Roblox\Player\RobloxPlayerBeta.exe"
-} else {
-	$bootstrapper = "Roblox"
-	$roblox = $robloxPath
-	Write-Warning "this will probably not work w/o bloxstrap lol"
-}
-
-$loop = 300
-$sleep = 0
-
 while ($true) {
 $other = ""
-while ($true) {
-	$answer = Read-Host "join a subplace [1] settings [2] about [3]"
-	if ($answer -eq "3") {
-		Write-Host "subplace.ps1 version 5. i think. made by kit. https://github.com/catb0x/subplace.ps1"
-    } elseif ($answer -eq "2") {
-		Write-Host "leave anything blank if u dont want to change it"
-		$bootstrapperQ = Read-Host "bootstrapper name? (default: detected (Bloxstrap or Roblox))"
-		if ($bootstrapperQ -ne "") { $bootstrapper = $bootstrapperQ }
-		$robloxQ = Read-Host "roblox location? (default: detected (Bloxstrap path or Roblox path))"
-		if ($robloxQ -ne "") { $roblox = $robloxQ }
-		$otherQ = Read-Host "instance/job id?"
-		if ($otherQ -ne "") { $other = "&gameInstanceId=${otherQ}" }
-		$loopQ = Read-Host "milliseconds to wait until repeating loops? (default: 300)"
-		if ($loopQ -ne "") { $loop = $loopQ }
-		$sleepQ = Read-Host "milliseconds to sleep until joining subplace? (default: 0) (do 100-300 if it breaks maybe?)"
-		if ($sleepQ -ne "") { $sleep = $sleepQ }
-	} elseif ($answer -eq "1") {
-		break
+
+while ($true) { # intro,,,
+
+$answer = Read-Host "join a subplace [1] settings [2] about [3]"
+if ($answer -eq "3") {
+	Write-Host "subplace.ps1 version 6. i think. made by kit. https://github.com/catb0x/subplace.ps1"
+} elseif ($answer -eq "2") {
+	Write-Host "enter ? if you want more information, leave blank if you dont want to change anything" -ForegroundColor Blue
+	Ask "bootstrapper" "bootstrapper name? (currently ${bootstrapper})" "default is detected, either Bloxstrap or Roblox. put your bootstrapper name here if you arent using default roblox or bloxstrap"
+	Ask "roblox" "full roblox path?" "currently ${roblox}. default is detected, put your RobloxPlayerBeta.exe path here if you arent using default roblox or bloxstrap"
+	IntAsk "loop" "loop delay? (currently ${loop})" "default: 500, but can change based on saved settings. how many ms to sleep before repeating loops"
+	IntAsk "sleep" "sleep delay? (currently ${sleep})" "default: 100. how many ms to sleep before loading the subplace. change depending on what happens"
+	BooleanAsk "manual" "enable manual mode? (currently ${manual})" "default: false. input boolean. enable if you want"
+	if (($isAdmin) -and ($gsudoExists)) { BooleanAsk "gsudoOptional" "enable gsudo loading mode? (currently ${gsudoOptional})" "default: depends. check source code." }
+	BooleanAsk "load" "save settings as file?" "saves settings on a file to be used later."
+	if ($load = $true) {
+		SaveSettings "loop", "sleep", "manual", "gsudoOptional"
+		$load = $false
 	}
+} elseif ($answer -eq "1") { break }
+
 }
 
-while ($true) {
-	$placeId = Read-Host "enter the place id"
-	if ($placeId -match "^\d+$") {
-		break
-	} else {
-		Write-Error "invalid place id. please enter a valid integer..."
-	}
+$placeId = Read-Host "enter the place id"
+if ($placeId -eq "nvm") {
+	Write-Host "not continuing..." -ForegroundColor Green
+	continue
+} elseif (!($placeId -match "^\d+$")) {
+	Write-Error "invalid place id. please enter a valid integer..."
+	continue
 }
-
-$request = Invoke-RestMethod -Uri "https://apis.roblox.com/universes/v1/places/${placeId}/universe"
-$universeId = $request.universeId
-$rootRequest = Invoke-RestMethod -Uri "https://games.roblox.com/v1/games?universeIds=${universeId}"
+try {
+	$request = Invoke-RestMethod -Uri "https://apis.roblox.com/universes/v1/places/${placeId}/universe"
+	$universeId = $request.universeId
+	$rootRequest = Invoke-RestMethod -Uri "https://games.roblox.com/v1/games?universeIds=${universeId}"
+} catch {
+	Write-Error "an error occured. either the place id isnt valid or roblox is down"
+	continue
+}
 $rootId = $null
 foreach ($place in $rootRequest.data) {
 	$rootId = $place.rootPlaceId
 	break
 }
 if ($placeId -eq $rootId) {
-	if ($universeId) {
-		Write-Verbose "universeid: ${universeId}"
-	} else {
-		Write-Warning "no universeid found for the given placeid..."
-		Exit
-	}
+	Write-Verbose "universeid: ${universeId}"
 	$places = Invoke-RestMethod -Uri "https://develop.roblox.com/v1/universes/${universeId}/places?limit=100"
 	if (!($places.data)) {
-		Write-Warning "no places found for the given universeid..."
-		Exit
+		Write-Warning "no places found for the given universeid... roblox is possibly down? idk"
+		continue
 	}
 	Write-Host "enter one of the following ids:"
 	foreach ($place in $places.data) {
 		Write-Host "$($place.name): $($place.id)" -ForegroundColor Blue
 	}
 	$subplace = Read-Host
+	if ($subplace -eq "nvm") {
+		Write-Host "not continuing..." -ForegroundColor Green
+		continue
+	}
+	JobId
 	Write-Host "joining the main place..." -ForegroundColor Green
 } else {
 	$subplace = $placeId
-	Write-Host "placeid has been found to be a subplace. joining..." -ForegroundColor Green
+	Write-Host "placeid has been found to be a subplace." -ForegroundColor Green
+	JobId
+	Write-Host "joining..." -ForegroundColor Green
 }
 
-
-
-Start-Process "roblox://experiences/start?placeId=${rootId}"
+if ($gsudoOptional -and ($gsudoMode -eq 1)) {
+	gsudo --integrity medium Start-Process "roblox://experiences/start?placeId=${rootId}"
+	Write-Verbose "joining with gsudo..."
+} else { Start-Process "roblox://experiences/start?placeId=${rootId}" }
 
 while ($true) {
     if (Is-WindowInForeground "Roblox") {
@@ -119,12 +215,26 @@ while ($true) {
 }
 
 Start-Sleep -Milliseconds $sleep
-netsh advfirewall firewall add rule name="subplace inbound" dir=in action=block program=$roblox enable=yes | Write-Verbose
-netsh advfirewall firewall add rule name="subplace outbound" dir=out action=block program=$roblox enable=yes | Write-Verbose
+
+if ($gsudoMode -eq 2) {
+	gsudo netsh advfirewall firewall add rule name="subplace inbound" dir=in action=block program=$roblox enable=yes | Write-Verbose
+	gsudo netsh advfirewall firewall add rule name="subplace outbound" dir=out action=block program=$roblox enable=yes | Write-Verbose
+	Write-Verbose "disabling internet with gsudo..."
+} else {
+	netsh advfirewall firewall add rule name="subplace inbound" dir=in action=block program=$roblox enable=yes | Write-Verbose
+	netsh advfirewall firewall add rule name="subplace outbound" dir=out action=block program=$roblox enable=yes | Write-Verbose
+}
+
 Write-Host "internet disabled. joining subplace, do not press retry yet" -ForegroundColor Green
 
-Start-Process "roblox://experiences/start?placeId=${subplace}${other}"
+if ($gsudoOptional -and ($gsudoMode -eq 1)) {
+	gsudo --integrity medium Start-Process "roblox://experiences/start?placeId=${subplace}${other}"
+	Write-Verbose "joining with gsudo..."
+} else { Start-Process "roblox://experiences/start?placeId=${subplace}${other}" }
 
+if ($manual) {
+	Start-Sleep -Milliseconds $sleep
+} else {
 while ($true) {
 	if (Is-WindowInForeground $bootstrapper) {
 		Write-Verbose "bootstrapper is in the foreground. continuing..."
@@ -143,8 +253,17 @@ while ($true) {
 	}
 	Start-Sleep -Milliseconds $loop
 }
+}
 
-netsh advfirewall firewall delete rule name="subplace inbound" | Write-Verbose
-netsh advfirewall firewall delete rule name="subplace outbound" | Write-Verbose
+if ($gsudoMode -eq 2) {
+	gsudo netsh advfirewall firewall delete rule name="subplace inbound" | Write-Verbose
+	gsudo netsh advfirewall firewall delete rule name="subplace outbound" | Write-Verbose
+	Write-Verbose "enabling internet with gsudo..."
+} else {
+	netsh advfirewall firewall delete rule name="subplace inbound" | Write-Verbose
+	netsh advfirewall firewall delete rule name="subplace outbound" | Write-Verbose
+}
+
 Write-Host "internet enabled. you can join now..." -ForegroundColor Green
+
 }
