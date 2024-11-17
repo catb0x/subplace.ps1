4a76db17f184379975ef4aabc3eb9369593087d9
kit/zyn ✯ <77354428+catb0x@users.noreply.github.com>
2024-11-17 11:26:33 +0100

    v7...

--- subplace.ps1
+++ subplace.ps1
@@ -1,13 +1,13 @@
 <#
 subplace.ps1
 by kit (https://github.com/catb0x/subplace.ps1)
-version 6 ^_^
+version 7 ^_^ (this time final fr.. i could make it v8 but ion want to)
 subject to the terms of the MPL 2.0, you can get a copy at http://mozilla.org/MPL/2.0/
 #>
 
 [CmdletBinding()] param ()
 
-$jsonPath = "$env:LOCALAPPDATA\subplace\settings.json"
+$jsonPath = "$env:LOCALAPPDATA\subplace\v7\settings.json"
 
 if (Get-Command "gsudo" -errorAction SilentlyContinue) {
     $gsudoExists = $true
@@ -39,42 +39,14 @@ function Is-WindowInForeground {
 
 function Ask {
     $question = Read-Host $args[1]
-	if ($question -eq "?") {
-		Write-Host $args[2] -ForegroundColor Blue
-		Ask $args[0] $args[1] $args[2]
-	} elseif ($question -ne "") {
-		Set-Variable -Name $args[0] -Value $question -Scope Script
-	}
-}
-
-function BooleanAsk {
-    $question = Read-Host $args[1]
 	$booleanAnswer = $null
 	if ($question -eq "?") {
 		Write-Host $args[2] -ForegroundColor Blue
 		BooleanAsk $args[0] $args[1] $args[2]
 	} elseif ([bool]::TryParse($question, [ref]$booleanAnswer)) {
 		Set-Variable -Name $args[0] -Value $booleanAnswer -Scope Script
-	}
-}
-
-function IntAsk {
-    $question = Read-Host $args[1]
-	if ($question -eq "?") {
-		Write-Host $args[2] -ForegroundColor Blue
-		BooleanAsk $args[0] $args[1] $args[2]
 	} elseif ($question -match "^\d+$") {
 		Set-Variable -Name $args[0] -Value ([int]$question) -Scope Script
-	}
-}
-
-function JobId {
-    $question = Read-Host "instance/job id? (leave blank if none)"
-	if ($question -eq "nvm") {
-		Write-Host "not continuing..." -ForegroundColor Green
-		$script:continue = $false
-	} elseif ($question -ne "") {
-		$other = "&gameInstanceId=${$question}"
 	}
 }
 
@@ -94,32 +66,21 @@ function SaveSettings {
 }
 
 function LoadSettings {
-    if (Test-Path $jsonPath) {
+    if ((Test-Path $jsonPath) -and !($reset)) {
         $varHash = Get-Content $jsonPath | ConvertFrom-Json
         foreach ($key in $varHash.PSObject.Properties.Name) {
             Set-Variable -Name $key -Value $varHash.$key -Scope Script -Force
         }
         Write-Verbose "settings loaded..."
     } else {
-        Write-Verbose "settings not found... making them rn"
+        Write-Verbose "no settings found or settings are being reset."
         $script:loop = 300
-        $script:sleep = 100
-        $script:manual = $false
+        $script:sleep = 750
         $script:gsudoOptional = $false
-        SaveSettings "loop", "sleep", "manual", "gsudoOptional"
+        SaveSettings "loop", "sleep", "gsudoOptional"
     }
 }
 LoadSettings
-
-$defPath = (Get-ItemProperty -Path "Registry::HKEY_CLASSES_ROOT\roblox-player\shell\open\command").'(default)' -replace '^"(.+?)".+', '$1'
-if ($defPath -eq "$($env:LOCALAPPDATA)\Bloxstrap\Bloxstrap.exe") {
-	$bootstrapper = "Bloxstrap"
-	$roblox = "$($env:LOCALAPPDATA)\Bloxstrap\Roblox\Player\RobloxPlayerBeta.exe"
-} else {
-	$bootstrapper = "Roblox"
-	$roblox = $defPath
-	Write-Warning "this will probably not work w/o bloxstrap lol"
-}
 
 Write-Host "            ___.          .__                                         ____ 
   ________ _\_ |__ ______ |  | _____    ____  ____      ______  _____/_   |
@@ -129,28 +90,27 @@ Write-Host "            ___.          .__                                       
      \/          \/|__|             \/     \/    \/  \/ |__|       \/      " -ForegroundColor Blue
 
 while ($true) {
-$other = ""
 
-while ($true) { # intro,,,
+$other = ""
+$reset = $false
 
 $answer = Read-Host "join a subplace [1] settings [2] about [3]"
 if ($answer -eq "3") {
-	Write-Host "subplace.ps1 version 6. i think. made by kit. https://github.com/catb0x/subplace.ps1"
+	Write-Host "subplace.ps1 version 7. idk. made by kit. https://github.com/catb0x/subplace.ps1"
+	continue
 } elseif ($answer -eq "2") {
 	Write-Host "enter ? if you want more information, leave blank if you dont want to change anything" -ForegroundColor Blue
-	Ask "bootstrapper" "bootstrapper name? (currently ${bootstrapper})" "default is detected, either Bloxstrap or Roblox. put your bootstrapper name here if you arent using default roblox or bloxstrap"
-	Ask "roblox" "full roblox path?" "currently ${roblox}. default is detected, put your RobloxPlayerBeta.exe path here if you arent using default roblox or bloxstrap"
-	IntAsk "loop" "loop delay? (currently ${loop})" "default: 500, but can change based on saved settings. how many ms to sleep before repeating loops"
-	IntAsk "sleep" "sleep delay? (currently ${sleep})" "default: 100. how many ms to sleep before loading the subplace. change depending on what happens"
-	BooleanAsk "manual" "enable manual mode? (currently ${manual})" "default: false. input boolean. enable if you want"
-	if (($isAdmin) -and ($gsudoExists)) { BooleanAsk "gsudoOptional" "enable gsudo loading mode? (currently ${gsudoOptional})" "default: depends. check source code." }
-	BooleanAsk "load" "save settings as file?" "saves settings on a file to be used later."
-	if ($load = $true) {
-		SaveSettings "loop", "sleep", "manual", "gsudoOptional"
-		$load = $false
+	Ask "reset" "reset all settings?" "reset all settings to default and save them"
+	if (!($reset)) {
+		Ask "loop" "loop delay? (currently ${loop})" "default: 500, but can change based on saved settings. how many ms to sleep before repeating loops"
+		Ask "sleep" "sleep delay? (currently ${sleep})" "default: 100. how many ms to sleep before loading the subplace. change depending on what happens"
+		if (($isAdmin) -and ($gsudoExists)) { Ask "gsudoOptional" "enable gsudo loading mode? (currently ${gsudoOptional})" "default: depends. check source code." }
 	}
-} elseif ($answer -eq "1") { break }
-
+	SaveSettings "loop", "sleep", "gsudoOptional"
+	Write-Host "settings automatically saved ^_^" -ForegroundColor Blue
+	continue
+} elseif ($answer -ne "1") {
+	continue
 }
 
 $placeId = Read-Host "enter the place id"
@@ -161,12 +121,13 @@ if ($placeId -eq "nvm") {
 	Write-Error "invalid place id. please enter a valid integer..."
 	continue
 }
+
 try {
 	$request = Invoke-RestMethod -Uri "https://apis.roblox.com/universes/v1/places/${placeId}/universe"
 	$universeId = $request.universeId
 	$rootRequest = Invoke-RestMethod -Uri "https://games.roblox.com/v1/games?universeIds=${universeId}"
 } catch {
-	Write-Error "an error occured. either the place id isnt valid or roblox is down"
+	Write-Error "an error occured. either the place id isnt valid or roblox is down."
 	continue
 }
 $rootId = $null
@@ -187,17 +148,22 @@ if ($placeId -eq $rootId) {
 	}
 	$subplace = Read-Host
 	if ($subplace -eq "nvm") {
-		Write-Host "not continuing..." -ForegroundColor Green
+		Write-Host "not joining..." -ForegroundColor Green
 		continue
 	}
-	JobId
-	Write-Host "joining the main place..." -ForegroundColor Green
 } else {
 	$subplace = $placeId
 	Write-Host "placeid has been found to be a subplace." -ForegroundColor Green
-	JobId
-	Write-Host "joining..." -ForegroundColor Green
 }
+
+$question = Read-Host "instance/job id? (leave blank if none)"
+if ($question -eq "nvm") {
+	Write-Host "not continuing..." -ForegroundColor Green
+	continue
+} elseif ($question -ne "") {
+	$other = "&gameInstanceId=${$question}"
+}
+Write-Host "joining..." -ForegroundColor Green
 
 if ($gsudoOptional -and ($gsudoMode -eq 1)) {
 	gsudo --integrity medium Start-Process "roblox://experiences/start?placeId=${rootId}"
@@ -215,45 +181,46 @@ while ($true) {
 }
 
 Start-Sleep -Milliseconds $sleep
-
 if ($gsudoMode -eq 2) {
-	gsudo netsh advfirewall firewall add rule name="subplace inbound" dir=in action=block program=$roblox enable=yes | Write-Verbose
-	gsudo netsh advfirewall firewall add rule name="subplace outbound" dir=out action=block program=$roblox enable=yes | Write-Verbose
-	Write-Verbose "disabling internet with gsudo..."
+	$roblox = gsudo Get-Process -Name RobloxPlayerBeta
+	$path = (gsudo Get-Process -Name RobloxPlayerBeta).path
+	Write-Verbose $path
+	gsudo Stop-Process -Name RobloxPlayerBeta
 } else {
-	netsh advfirewall firewall add rule name="subplace inbound" dir=in action=block program=$roblox enable=yes | Write-Verbose
-	netsh advfirewall firewall add rule name="subplace outbound" dir=out action=block program=$roblox enable=yes | Write-Verbose
+	$roblox = Get-Process -Name RobloxPlayerBeta
+	$path = (Get-Process -Name RobloxPlayerBeta).path
+	Write-Verbose $path
+	Stop-Process -Name RobloxPlayerBeta
 }
-
-Write-Host "internet disabled. joining subplace, do not press retry yet" -ForegroundColor Green
 
 if ($gsudoOptional -and ($gsudoMode -eq 1)) {
 	gsudo --integrity medium Start-Process "roblox://experiences/start?placeId=${subplace}${other}"
 	Write-Verbose "joining with gsudo..."
-} else { Start-Process "roblox://experiences/start?placeId=${subplace}${other}" }
-
-if ($manual) {
-	Start-Sleep -Milliseconds $sleep
 } else {
+	Start-Process "roblox://experiences/start?placeId=${subplace}${other}"
+}
+
 while ($true) {
-	if (Is-WindowInForeground $bootstrapper) {
-		Write-Verbose "bootstrapper is in the foreground. continuing..."
-		break
-	} else {
-		Write-Verbose "bootstrapper is not in the foreground."
-	}
-	Start-Sleep -Milliseconds $loop
+    if (Is-WindowInForeground "Roblox") {
+        Write-Verbose "roblox is in the foreground. enabling internet..."
+        break
+    } else {
+        Write-Verbose "roblox is not in the foreground."
+    }
+    Start-Sleep -Milliseconds $loop
 }
-while ($true) {
-	if (Is-WindowInForeground $bootstrapper) {
-		Write-Verbose "bootstrapper is not in the background."
-	} else {
-		Write-Verbose "bootstrapper is in the background. enabling internet..."
-		break
-	}
-	Start-Sleep -Milliseconds $loop
+
+if ($gsudoMode -eq 2) {
+	gsudo netsh advfirewall firewall add rule name="subplace inbound" dir=in action=block program=$path enable=yes | Write-Verbose
+	gsudo netsh advfirewall firewall add rule name="subplace outbound" dir=out action=block program=$path enable=yes | Write-Verbose
+	Write-Verbose "disabling internet with gsudo..."
+} else {
+	netsh advfirewall firewall add rule name="subplace inbound" dir=in action=block program=$path enable=yes | Write-Verbose
+	netsh advfirewall firewall add rule name="subplace outbound" dir=out action=block program=$path enable=yes | Write-Verbose
 }
-}
+
+Write-Host "internet disabled. joined subplace, do not press retry yet" -ForegroundColor Green
+Start-Sleep -Milliseconds $sleep
 
 if ($gsudoMode -eq 2) {
 	gsudo netsh advfirewall firewall delete rule name="subplace inbound" | Write-Verbose
